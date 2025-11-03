9f7dd33216f013cf2a84c0d7e4d50ccf3f05e188
Dr.rabi3 <abdalrazekmohmed6@gmail.com>
2025-11-03 18:08:54 +0200

    deploy prepration

--- CPStanding/src/main.jsx
+++ CPStanding/src/main.jsx
@@ -6,6 +6,7 @@ import ContestStanding from './components/ContestStanding.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import RootLayout from './components/RootLayout.jsx'
 import LandingPage from './components/LandingPage.jsx'
+import { Analytics } from "@vercel/analytics/next"
 
 const router = createBrowserRouter([
   {
@@ -31,5 +32,6 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router}/>
+    <Analytics />
   </StrictMode>,
 )
