3febff3622d0f504894eccad92d47a5eaec0b59f
Dr.rabi3 <abdalrazekmohmed6@gmail.com>
2025-11-03 23:39:57 +0200

    fix vercel routing

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,14 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://competitive-programming-standing.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
